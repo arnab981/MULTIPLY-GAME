0c5df863fe824a02ccf62253eaf8319f725a7e7d
arnab981 <126707756+arnab981@users.noreply.github.com>
2023-03-05 17:52:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const quesEl = document.getElementById("ques");
+
+const inputEl = document.getElementById("input");
+
+const formEl = document.getElementById("form");
+
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+    score = 0;
+}
+
+/*scoreEl.innerText = `score: ${score}`;
+
+quesEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+const result = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+    const userAns = +inputEl.Value;
+    if (userAns === result) {
+        score++;
+        updateLocalStorage();
+        }
+        else{
+            score--;
+            updateLocalStorage();
+        }
+});
+
+function updateLocalStorage() {
+    localStorage.setItem("score", JSON.stringify(score));
+}*/
+
+scoreEl.innerText = `score: ${score}`;
+
+quesEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+  const userAns = +inputEl.value;
+  if (userAns === correctAns) {
+    score++;
+    updateLocalStorage();
+   } 
+   else{
+    score--;
+    updateLocalStorage();
+  }
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
